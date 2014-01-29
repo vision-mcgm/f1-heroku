9136629e6b23cb849669432057b5193b3c5dc73e
fusionlove <fusionlove@gmail.com>
2014-01-29 03:44:46 +0000

    db fix

--- f1/settings.py
+++ f1/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+
 #BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -92,8 +94,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
