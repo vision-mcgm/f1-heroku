a757abcc329b01641f668f44d6e1f24885c91d2c
fusionlove <fusionlove@gmail.com>
2014-02-19 02:22:28 +0000

    deploy

--- f1/urls.py
+++ f1/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	#NO EOL matchers in REs which get included!
+  #NO EOL matchers in REs which get included!
     # Examples:
     # url(r'^$', 'f1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -14,16 +14,26 @@ urlpatterns = patterns('',
    # url(r'^polls/', include('f1_a1.urls')),url(r'^$', views.welcome, name='welcome'),
   url(r'^admin/', include(admin.site.urls)),
 
- #  url(r'^wall/', include('f1_a1.urls', namespace="f1_a1")),
- # # url(r'^home/', include('f1_a1.urls', namespace="f1_a1"),name='home'),
- # url(r'^home/', 'f1_a1.views.home',name='home'),
- # url(r'^login/$', 'django.contrib.auth.views.login'),
- # url(r'^logout/', 'f1_a1.views.logout_view',name='logout'),
- # url(r'^person/(\w*)$', 'f1_a1.views.person', name='person'),
- # url(r'^message/$', 'f1_a1.views.message', name='message'),
-   url(r'^welcome$', 'f1_a1.views.welcome',name='welcome'),
- #   #Any
+  url(r'^wall/', include('f1_a1.urls', namespace="f1_a1")),
+ # url(r'^home/', include('f1_a1.urls', namespace="f1_a1"),name='home'),
+ url(r'^home$', 'f1_a1.views.home',name='home'),
+ url(r'^login$', 'f1_a1.views.login',name='login'),
+ url(r'^join$', 'f1_a1.views.join',name='join'),
+ #url(r'^login2$', 'django.contrib.auth.views.login',name='login2'),
+ url(r'^logout/', 'f1_a1.views.logout_view',name='logout'),
+ url(r'^welcome$', 'f1_a1.views.welcome',name='welcome'),
+ url(r'^person/(\w*)$', 'f1_a1.views.person', name='person'),
+ #Post message: parent convo and parent message
+ url(r'^post$', 'f1_a1.views.post', name='post'),
+  url(r'^converse$', 'f1_a1.views.converse', name='converse'),
+  url(r'^reply$', 'f1_a1.views.reply', name='reply'),
+
+#AJAX request handlers
+ url(r'^getMessagesByUID$', 'f1_a1.views.getMessagesByUID', name='getMessagesByUID'),
+  #url(r'^getMessagesByConvoID$', 'f1_a1.views.getMessagesByConvoID', name='getMessagesByConvoID'),
+
+ 
+ url(r'^test(\d*)$', 'f1_a1.views.test', name='test'),
+
   url(r'', 'f1_a1.views.welcome'),
-   url(r'^login$', 'f1_a1.views.login',name='login'),
-   url(r'^logout/', 'f1_a1.views.logout_view',name='logout'),
 )
--- f1_a1/views.py
+++ f1_a1/views.py
@@ -9,16 +9,55 @@ from django.contrib.auth.models import User
 from json import dumps
 import logging
 
+log = logging.getLogger(__name__)
 
+#HTTP real page view requests
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('f1_a1/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
+# Create your views here.
+
+
+def vote(request):
+	return HttpResponse("You're voting on poll")
+
+def wall(request):
+	if request.user.is_authenticated():
+		logger = logging.getLogger(__name__)
+		logger.error('Starting wall view')
+		posts=WallPost.objects.all()
+		template = loader.get_template('f1_a1/wall.html')
+		c=RequestContext(request,{'posts':posts})
+		return HttpResponse(template.render(c))
+	else:
+		return HttpResponse("You are not logged in!")
+
+def wallpost(request):
+	thetext=request.POST['text']
+	wp=WallPost(text=thetext)
+	wp.save()
+	return HttpResponseRedirect(reverse('f1_a1:wall', ))
 
 def welcome(request):
 	template = loader.get_template('f1_a1/welcome.html')
 	context = RequestContext(request, {})
 	return HttpResponse(template.render(context))
 
-def logout_view(request):
-	logout(request)
-	return HttpResponse("Logged out")
+
+def home(request):
+	if request.user.is_authenticated():
+		template = loader.get_template('f1_a1/home.html')
+		context = RequestContext(request,{})
+		return HttpResponse(template.render(context))
+	else:
+		return HttpResponseRedirect('/welcome')
+
+
 
 def login(request):
 	username = request.POST['handle']
@@ -32,3 +71,150 @@ def login(request):
 			return HttpResponse("Problem logging in")
 	else:
 		return HttpResponse("Problem authenticating")
+
+def join(request):
+	handle = request.REQUEST.get('handle', None)
+	userPass = request.REQUEST.get('password', None)
+	userMail = request.REQUEST.get('email', None)
+	name = request.REQUEST.get('name', None)
+	user = User.objects.create_user(handle, userMail, userPass)
+	user.save()
+	p=Person(handle=handle,email=userMail,name=name,uid=user.id)
+	p.save()
+	return HttpResponse("User created")
+   
+
+def person(request,id):
+	if Person.objects.filter(handle=id).count():
+		person=Person.objects.get(handle=id)
+		template = loader.get_template('f1_a1/person.html')
+		context = RequestContext(request, {'person':person})
+		return HttpResponse(template.render(context))
+	else:
+		return HttpResponse('No person by that name: '+id)
+
+def message(request):
+	return HttpResponse('This is a message!')
+
+#AJAX GET requests
+
+def getMessagesByUID_old(request):
+	#GET request by Ajax
+	if request.user.is_authenticated():
+		uid=request.user.id
+		#Get convos
+		convos=Conversation.objects.filter(uid=uid)	
+		jConvos=[]
+		for c in convos:
+			messages=Message.objects.filter(pConvo=c)
+			jMsgs=[]
+			for m in messages:
+				jMsgs.append({'text':m.text,'name':m.name,'id':m.id})
+			jConvos.append(jMsgs)
+		return HttpResponse(dumps(jConvos))
+	else:
+		return HttpResponse('This page has no messages.')
+
+def getMessagesByUID(request):
+	#GET request by Ajax
+	if request.user.is_authenticated():
+		uid=request.user.id
+		#Get convos
+		convos=Conversation.objects.filter(uid=uid)	
+		jConvos=[]
+		for c in convos:
+			messages=Message.objects.filter(convoID=c.id)
+			log.error(str(c.id)+'Posting message')
+			jMsgs=[]
+			for m in messages:
+				jMsgs.append({'text':m.text,'name':m.name,'id':m.id,'parent':m.parentID})
+			jConvos.append({'msgs':jMsgs,'convoID':c.id})
+		return HttpResponse(dumps(jConvos))
+	else:
+		return HttpResponse('This page has no messages.')
+
+
+
+
+def converse(request):
+	#return HttpResponse(request.GET.get('type'))
+	#When messages come into the server, we need to check a) authentication
+#b that they're not abusing.
+	log.error('Posting message')
+	if request.user.is_authenticated():
+		text=request.GET['text']
+		uid=request.user.id
+		if len(text):
+			c=Conversation(uid=uid)
+			c.save()
+			replyTo(request,c.id,2)
+		return HttpResponse(c.id)
+
+def reply(request):
+	#return HttpResponse(request.GET.get('type'))
+	#When messages come into the server, we need to check a) authentication
+#b that they're not abusing.
+	log.error('Posting message')
+	if request.user.is_authenticated():
+		text=request.GET['text']
+		uid=request.user.id
+		
+		if len(text):
+			replyTo(request,request.GET['convoID'],request.GET['parentID'])
+		return HttpResponse(request.GET['convoID'])
+		
+
+
+def buildConversation(request):
+	#Should return an HTTPResponse with the HTML format of the conversation
+	messages=Message.objects.get(pConvo=convoID)
+	template = loader.get_template('f1_a1/convoView.html')
+	context = RequestContext(request, {'person':person})
+	return HttpResponse(template.render(context))
+
+
+
+def test(request,testNo):
+	template = loader.get_template('f1_a1/test'+testNo+'.html')
+	context = RequestContext(request, {})
+	return HttpResponse(template.render(context))
+	
+
+def logout_view(request):
+	logout(request)
+	return HttpResponse("Logged out")
+
+
+def csrf_failure(request, reason):
+	return HttpResponse("CSRF failure")
+
+#Non-HTTP-request functions
+
+def replyTo(request,convoID,parentID):
+	#Add child message to a particular message
+	if request.user.is_authenticated():
+		person=Person.objects.get(uid=request.user.id)
+		m=Message(uid=request.user.id,text=request.GET['text'],
+			handle=person.handle,name=person.name,
+			parentID=parentID,
+			convoID=convoID)
+#OPT
+#pConvo may be unused
+		m.save()
+#SEC
+#We need to check that the message and the convo go together, to avoid malicious posts.
+
+
+def post(request,convo):
+	log.error('Posting message')
+	if request.user.is_authenticated():
+		#Fill this in correctly:
+		uid=request.user.id
+		person=Person.objects.get(uid=request.user.id)
+
+		m=Message(uid=request.user.id,text=request.GET['text'],
+			handle=person.handle,name=person.name,pConvo=convo)
+		m.save()	
+		
+	else:
+		return HttpResponse('Not authenticated.')
