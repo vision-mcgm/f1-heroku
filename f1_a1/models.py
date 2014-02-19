96e93afd9687082177c0fe28aa50c54afcc14d40
fusionlove <fusionlove@gmail.com>
2014-02-19 02:02:44 +0000

    test

--- f1_a1/models.py
+++ f1_a1/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from uuid import uuid4
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __unicode__(self):
-    	return self.question
+     return self.question
     def was_published_recently(self):
-    	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
@@ -18,7 +19,7 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __unicode__(self):
-    	return self.choice_text
+     return self.choice_text
 
 class WallPost(models.Model):
     text=models.CharField(max_length=1000)
@@ -33,9 +34,16 @@ class WallPost(models.Model):
 class Person(models.Model):
     handle=models.CharField(max_length=1000)
     name=models.CharField(max_length=1000)
+    email=models.EmailField(max_length=128)
 
     def __unicode__(self):
         return self.name
+
+
+class Conversation(models.Model):
+    text=models.CharField(max_length=1000)
+    convoID=models.CharField(max_length=100, blank=True, unique=True, default=uuid4)
+    #So that uuid is reevaluated every time
 
 
 
@@ -48,7 +56,9 @@ class Message(models.Model):
     #Author numerical ID
     author=models.IntegerField(default=0)
     text=models.CharField(max_length=1000)
-    parentConversation=models.ForeignKey(ProtoConversation)
+    pConvo=models.ForeignKey(Conversation)
+   # pMessage=models.ForeignKey(Message)
+
 
 
 class Audience(models.Model):
